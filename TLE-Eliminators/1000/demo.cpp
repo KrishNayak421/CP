58154b5d6aafcaf35c41a296105d00504655e28e
KrishNayak421 <krishnayak421@gmail.com>
2025-03-15 18:35:47 +0530

    comm1

--- TLE-Eliminators/1000/demo.cpp
+++ TLE-Eliminators/1000/demo.cpp
@@ -1,42 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define ll long long 
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
 
-void solve() {
-    ll n, d;
-    cin >> n >> d;
+void solve(){
+    ll n,x;
+    cin>>n>>x;
     vector<ll> a(n);
-    for (ll i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-    sort(a.rbegin(), a.rend());
+    for(ll i=0;i<n;i++) cin>>a[i];
 
-    ll count = 0;
-    ll i = 0;
-    while (i < n) {
-        ll p = a[i];
-        ll required = d + 1;
-        ll k;
-        if(p != 0) k = (required + p - 1) / p;
-        else k = 0;
-        if (i + k <= n) {
-            count++;
-            i += k;
-        } else {
-            break;
-        }
-    }
-    cout << count << endl;
+    
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int t = 1;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+    ll t;
+    cin>>t;
+    while(t--) solve();
+    
     return 0;
 }
